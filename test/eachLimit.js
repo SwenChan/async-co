4afc5976d3571a4798415691d8e3228cc444abf3
Swen <a274979395@163.com>
2016-09-02 01:28:47 +0800

    add test

--- test/eachLimit.js
+++ test/eachLimit.js
@@ -1,19 +1,27 @@
 'use strict';
 var mocha = require('mocha')
-var coMocha = require('co-mocha')
-
-coMocha(mocha)
 const should = require('should');
 const Promise = require('bluebird');
 const eachLimit = require('../lib/eachLimit');
+const eachOfLimit = require('../lib/eachOfLimit');
 
 describe('array => ', function () {
   it('each', function * () {
     function * thunk (value, key) {
       yield Promise.delay(1000);
+      console.log('key value', key, value)
       return value;
     }
     const result = yield eachLimit([1,2,3,4], 2, thunk);
     console.log(result);
   });
+  it('eachOf', function * () {
+    function * thunk (value, key) {
+      yield Promise.delay(1000);
+      console.log(key, value);
+      return [key, value];
+    }
+    const result = yield eachOfLimit([1,2,3,4], 2, thunk);
+    console.log(result);
+  })
 });
